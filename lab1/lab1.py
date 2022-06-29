5a843559fd382ce7a44acf1c6361e46212cafc92
Gheorghe24 <gheorghe231101@yahoo.com>
2022-06-29 17:55:52 +0300

    fdsd

---
+++ lab1/lab1.py
@@ -0,0 +1,13 @@
+n=int(input("Enter a number"))
+
+if n%2 == 0:
+    print("odd")
+else:
+    print("even")
+
+list1 = [1, 4, 9, 25]
+for elem in list1:
+    if elem%2 == 0:
+        print("odd")
+    else:
+        print("even")

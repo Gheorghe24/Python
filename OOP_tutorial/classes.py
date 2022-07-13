ebcc74a632249581f01cb0ef67ffdd2c1aec6244
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-13 20:36:00 +0300

    student and course example

--- OOP_tutorial/classes.py
+++ OOP_tutorial/classes.py
@@ -1,16 +1,45 @@
 
 # Method = function that goes inside of a class
-class Dog:
-
-    def __init__(self, name, age) :
+class Student:
+    def __init__(self, name, age, grade) -> None:
         self.name = name
         self.age = age
+        self.grade = grade # 0 - 100
 
-    def get_name(self):
-        return self.name
+    def get_grade(self) :
+        return self.grade
 
-d = Dog("Tim", 34)  
-d2 = Dog("Bill", 12)
+class Course :
+    def __init__(self,name, max_students) -> None:
+        self.name = name
+        self.max_students = max_students
+        self.students = []
 
-print(d.get_name())
-print(d2.name)
+    def add_student(self, student) :
+        if len(self.students) < self.max_students :
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self) :
+        value = 0
+        for student in self.students :
+            value += student.get_grade()
+
+        return value/len(self.students)
+
+
+
+    
+
+s1 = Student("Tim", 19, 95)
+s2 = Student("Bill", 19, 75)
+s3 = Student("Jill",19, 65)
+
+course = Course("Science", 2)
+course.add_student(s1)
+course.add_student(s2)
+print(course.get_average_grade())
+
+        
+

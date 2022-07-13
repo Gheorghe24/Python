60efe9526768ea01c565fe01cbf966d4e3c14684
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-13 22:31:20 +0300

    init class atribute

--- OOP_tutorial/inheritance.py
+++ OOP_tutorial/inheritance.py
@@ -15,14 +15,20 @@ class Cat(Pet):
         print("meow")
 
 class Dog(Pet):
+    def __init__(self, name, age, color) -> None:
+        self.color = color
+        super().__init__(name, age)
 
     def speak(self):
         print("bark")
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and I am {self.color}" )
 
 p = Pet("Tim", 19)
 p.show()
 c = Cat("Bill", 34)
 c.show()
-d = Cat("Jill", 14)
+d = Dog("Jill", 14, "brown")
 d.show()
 d.speak()

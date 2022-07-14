da2556686fcda282cf8defba739c36b53cb119b1
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-14 11:23:13 +0300

    first exercise

---
+++ tasks/1.py
@@ -0,0 +1,11 @@
+# 1. Write a Python program to print the following string in a specific format (see the output). Go to the editor
+# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" Output :
+
+# Twinkle, twinkle, little star,
+#	How I wonder what you are! 
+#		Up above the world so high,   		
+#		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+#	How I wonder what you are
+
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.s\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")

3df4a52b1df3a41d38d674f09b55cb20b30610e6
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-16 12:20:31 +0300

    again some exercises

--- tasks/39_40_41.py
+++ tasks/39_40_41.py
@@ -7,12 +7,22 @@
 # r = the annual interest rate expressed as a decimal;
 # n = the number of times interest is paid each year;
 # t = time in years.
+
+from urllib3 import Retry
+
+
 def value(amt,int,years):
     return amt*((1+(0.01*int)) ** years)
 
 
-
 # # 40. Write a Python program to compute the distance between the points (x1, y1) and (x2, y2). 
-
+import math
+def distance(x1,y1,x2,y2):
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
 # # 41. Write a Python program to check whether a file exists. 
+
+import os.path
+def exists(name):
+
+    return os.path.exists(name)

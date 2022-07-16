265cf9159099b641f062212855602911d3279c5a
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-16 11:37:02 +0300

    again some exercises

---
+++ tasks/39_40_41.py
@@ -0,0 +1,18 @@
+# # 39. Write a Python program to compute the future value of a specified principal amount, rate of interest, and a number of years. 
+# # Test Data : amt = 10000, int = 3.5, years = 7
+# # Expected Output : 12722.79
+# The formula for future value with compound interest is FV = P(1 + r/n)^nt.
+# FV = the future value;
+# P = the principal;
+# r = the annual interest rate expressed as a decimal;
+# n = the number of times interest is paid each year;
+# t = time in years.
+def value(amt,int,years):
+    return amt*((1+(0.01*int)) ** years)
+
+
+
+# # 40. Write a Python program to compute the distance between the points (x1, y1) and (x2, y2). 
+
+
+# # 41. Write a Python program to check whether a file exists. 

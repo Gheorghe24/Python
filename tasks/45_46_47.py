79aa7d74467c09da7ee9f930986e9c709f3dd169
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-17 13:38:37 +0300

    again some exercises

---
+++ tasks/45_46_47.py
@@ -0,0 +1,11 @@
+# 45. Write a Python program to call an external command. 
+
+import subprocess
+return_code = subprocess.call(['ping', 'localhost'])
+print("Output of call() : ", return_code)
+
+
+# 46. Write a python program to get the path and name of the file that is currently executing. 
+
+
+# 47. Write a Python program to find out the number of CPUs using. 

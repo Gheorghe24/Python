3fd72d4a35915765ed9b1cc1e0ede0ce5aa2b904
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-19 10:24:04 +0300

    some random code

--- code_bro12hrs/math_simple.py
+++ code_bro12hrs/math_simple.py
@@ -1,8 +1,27 @@
-pi = 3.14
-x = 1
-y = 2
+# str.format() = optional method that gives users more control when displaying
 
-import math
+from numpy import number
 
 
+animal = "cow"
+item = "moon"
 
+print("The {} jumped over the {}".format(animal,item))
+
+text = "The {} jumped over the  {}"
+
+print(text.format(animal,item))
+
+name = "Bro"
+
+print("Hello, my name is {:<10}. Nice to meet you".format(name))
+print("Hello, my name is {:10}. Nice to meet you".format(name))
+print("Hello, my name is {:>10}. Nice to meet you".format(name))
+print("Hello, my name is {:^10}. Nice to meet you".format(name))
+
+number = 3.1415
+numberr = 1000
+print(f"The number pi is {number:.2f}")
+print(f"The number is {numberr:b}")
+print(f"The number is {numberr:o}")
+print(f"The number pi is {numberr:X}")

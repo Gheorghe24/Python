b558d8c8ac266bf89692ab218ec661d975f24b8e
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-21 16:13:32 +0300

    example of class exercise

---
+++ classes/script.py
@@ -0,0 +1,26 @@
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+myobjecty = MyClass()
+
+myobjecty.variable = "yackity"
+
+# Then print out both values
+print(myobjectx.variable)
+print(myobjecty.variable)
+
+
+class NumberHolder:
+
+   def __init__(self, number):
+       self.number = number
+
+   def returnNumber(self):
+       return self.number
+
+var = NumberHolder(7)
+print(var.returnNumber()) #Prints '7'

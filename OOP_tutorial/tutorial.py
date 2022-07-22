8850db91d5c42052544b34f29da28241416164e8
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-23 00:08:21 +0300

    types of attributes

--- OOP_tutorial/tutorial.py
+++ OOP_tutorial/tutorial.py
@@ -1,6 +1,7 @@
 
 class Item:
     pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
 
     def __init__(self, name: str, price: float, quantity = 0) -> None:
         #Run validation to the received arguments
@@ -12,21 +13,21 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        #Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
-        self.price *= Item.pay_rate
+        self.price *= self.pay_rate
 
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 500, 5)
+item4 = Item("Mouse", 200, 3)
+item5 = Item("Keyboard", 75, 10)
 
-print(item1.price)
-item1.apply_discount()
-print(item1.price)
-
-item2.pay_rate = 0.7
-print(item2.price)
-item2.apply_discount()
-print(item2.price)
- 
+print(Item.all)
+for x in Item.all:
+    print(x.name)

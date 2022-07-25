4b6be6ccb8409fd9acea0a6f7b9c0b7db2a5168d
Gheorghe24 <gheorghe231101@yahoo.com>
2022-07-25 15:42:44 +0300

    Just the EINWAY projects

--- OOP_tutorial/CodeCampOOP/main.py
+++
@@ -1,6 +0,0 @@
-from item import Item
-from phone import Phone
-
-item1 = Item("My", 750, 6)
-item1.send_email()
-# print(item1.price)
--- tasks/2.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to get the Python version you are using. 
-
-import sys
-
-print(sys.version)
